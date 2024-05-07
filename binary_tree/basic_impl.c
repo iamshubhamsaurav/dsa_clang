6d2f2f1e88415d152e84d58855d91ba0603010bb
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-05-07 21:10:17 +0530

    buildTreeFromPreordered nodes function added in basic impl

--- binary_tree/basic_impl.c
+++ binary_tree/basic_impl.c
@@ -7,30 +7,38 @@ struct Node {
     struct Node *right;
 };
 
+int INDEX = -1;
+struct Node* buildTreePreOrder(int nodes[]) {
+    INDEX++;
+    if (nodes[INDEX] == -1) {
+        return NULL;
+    }
+    
+    struct Node *root = (struct Node*) malloc(sizeof(struct Node));
+    root->data = nodes[INDEX];
 
+    root->left = buildTreePreOrder(nodes);
+    root->right = buildTreePreOrder(nodes);
 
+    return root; 
+}
 
+void preOrderTraversal(struct Node* root) {
+    if(root == NULL) return;
+    printf(" %d ", root->data);
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
 
 int main() {
 
-    struct Node* root = (struct Node*) malloc(sizeof(struct Node));
-    root->data = 1;
-    root->left = NULL;
-    root->right = NULL;
-
-    struct Node* leftNode = (struct Node*) malloc(sizeof(struct Node));
-    leftNode->data = 2;
-    leftNode->left = NULL;
-    leftNode->right = NULL;
-
-    root->left = leftNode;
+    struct Node* root = NULL;
+    int nodes[] =  {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    root = buildTreePreOrder(nodes);
+    preOrderTraversal(root);
     
-    struct Node* rightNode = (struct Node*) malloc(sizeof(struct Node));;
-    rightNode->data = 3;
-    rightNode->left = NULL;
-    rightNode->right = NULL;
 
-    root->right = rightNode;
+    
 
 
 
